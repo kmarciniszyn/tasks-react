57478991a84ab2f7382ea1281183c4dfcf3c5794
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-01-11 22:40:43 +0100

    refactored code in Author.js

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -3,24 +3,22 @@ import Container from "../../common/Container";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 
-export default () => {
-    return (
-        <>
-            <Container>
-                <Header
-                    title="O autorze"
-                />
-                <Section
-                    title="Karolina Marciniszyn"
-                    body={
-                        <p>Moje studia dały mi inspirację do nauki programowania. Wybrałam specjalność <b>informatyka
-                            medyczna</b> na kierunku <strong>inżynieria biomedyczna</strong>. Choć sam kierunek 
-                            nieszczególnie przypadł mi do gustu, tak zajęcia z programowania dawały mi bardzo dużo radości 
-                            i satysfakcji z efektów końcowych. Nie wyobrażam sobie iść w innym kierunku niż programowanie,
-                             które stało się moim marzeniem, planem na przyszłość, do którego stopniowo dążę.</p>
-                    }
-                />
-            </Container>
-        </>
-    )
-}
+const Author = () => (
+    <Container>
+        <Header
+            title="O autorze"
+        />
+        <Section
+            title="Karolina Marciniszyn"
+            body={
+                <p>Moje studia dały mi inspirację do nauki programowania. Wybrałam specjalność <b>informatyka
+                    medyczna</b> na kierunku <strong>inżynieria biomedyczna</strong>. Choć sam kierunek
+                    nieszczególnie przypadł mi do gustu, tak zajęcia z programowania dawały mi bardzo dużo radości
+                    i satysfakcji z efektów końcowych. Nie wyobrażam sobie iść w innym kierunku niż programowanie,
+                    które stało się moim marzeniem, planem na przyszłość, do którego stopniowo dążę.</p>
+            }
+        />
+    </Container>
+);
+
+export default Author;
